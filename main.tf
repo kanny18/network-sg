6569bc4050283049f7cc2e41f551b15acb8ca819
Kapoor <kunal.kapoor@refinitiv.com>
2022-06-18 16:02:06 +0530

    dev workflow

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "terragitactions"
-    workspaces { prefix = "network-" }
+    workspaces { prefix = "network-sg" }
   }
 }
 
