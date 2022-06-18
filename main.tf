3e51d21c5303462f6528323bac0ba8c84445ef07
Kapoor <kunal.kapoor@refinitiv.com>
2022-06-18 16:00:42 +0530

    dev workflow

--- main.tf
+++ main.tf
@@ -16,15 +16,15 @@ module "http_sg" {
   source = "github.com/nitheesh86/terraform-modules/modules/sg"
 
   name        = "http-sg"
-  description = "Security group with HTTP port open for everyone, and HTTPS open just for the default security group"
-  vpc_id      = vpc-0bcfc7dacc703ade8
+  description = "Security  HTTP port open for everyone, and HTTPS open just for the default security group"
+  vpc_id      = vpc-0e6620fb48a4eefb7
 
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   ingress_with_source_security_group_id = [
     {
       rule                     = "https-443-tcp"
-      source_security_group_id = data.aws_security_group.default.id
+      source_security_group_id = "sg-050d4bc0c53e75d50"
     },
   ]
 }
